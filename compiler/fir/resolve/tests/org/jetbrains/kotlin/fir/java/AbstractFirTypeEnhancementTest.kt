9136b1f3667603a707ffc00bde778af4b0a0a8c5
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2019-02-27 18:15:51 +0300

    FIR: implement Java fields (in provider, type enhancement, scopes)

--- compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/java/AbstractFirTypeEnhancementTest.kt
+++ compiler/fir/resolve/tests/org/jetbrains/kotlin/fir/java/AbstractFirTypeEnhancementTest.kt
@@ -23,6 +23,7 @@ import org.jetbrains.kotlin.fir.FirRenderer
 import org.jetbrains.kotlin.fir.createSession
 import org.jetbrains.kotlin.fir.declarations.FirDeclaration
 import org.jetbrains.kotlin.fir.java.declarations.FirJavaClass
+import org.jetbrains.kotlin.fir.java.declarations.FirJavaField
 import org.jetbrains.kotlin.fir.java.declarations.FirJavaMethod
 import org.jetbrains.kotlin.fir.java.scopes.JavaClassEnhancementScope
 import org.jetbrains.kotlin.fir.resolve.FirScopeProvider
@@ -187,6 +188,15 @@ abstract class AbstractFirTypeEnhancementTest : KtUsefulTestCase() {
                             when (declaration) {
                                 is FirJavaMethod -> enhancementScope.processFunctionsByName(declaration.name) { symbol ->
                                     val enhanced = (symbol as? FirFunctionSymbol)?.fir
+                                    if (enhanced != null && enhanced !in renderedDeclarations) {
+                                        enhanced.accept(renderer, null)
+                                        renderer.newLine()
+                                        renderedDeclarations += enhanced
+                                    }
+                                    ProcessorAction.NEXT
+                                }
+                                is FirJavaField -> enhancementScope.processPropertiesByName(declaration.name) { symbol ->
+                                    val enhanced = (symbol as? FirPropertySymbol)?.fir
                                     if (enhanced != null && enhanced !in renderedDeclarations) {
                                         enhanced.accept(renderer, null)
                                         renderer.newLine()
