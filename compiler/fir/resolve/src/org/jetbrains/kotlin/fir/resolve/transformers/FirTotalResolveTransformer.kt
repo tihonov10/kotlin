dbde214f6ac9afe7b876403b4c4535ac9c3ed65a
Simon Ogorodnik <Simon.Ogorodnik@jetbrains.com>
2019-03-12 17:37:37 +0300

    Implement deep implicit types resolution

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirTotalResolveTransformer.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/resolve/transformers/FirTotalResolveTransformer.kt
@@ -14,6 +14,7 @@ class FirTotalResolveTransformer {
         FirImportResolveTransformer(),
         FirTypeResolveTransformer(),
         FirStatusResolveTransformer(),
+        FirImplicitTypeBodyResolveTransformerAdapter(),
         FirBodyResolveTransformerAdapter()
     )
 
